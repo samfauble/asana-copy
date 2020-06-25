052fae9261fc8ddb605cc30a4784cc77752f9fc1
Sam <samfauble@gmail.com>
2020-06-25 16:26:14 -0300

    redirect_url constant

--- authentication.js
+++ authentication.js
@@ -112,7 +112,8 @@ module.exports = {
         params: {
           client_id: '{{process.env.CLIENT_ID}}',
           state: '{{bundle.inputData.state}}',
-          redirect_uri: '{{bundle.inputData.redirect_uri}}',
+          redirect_uri,
+          //redirect_uri: '{{bundle.inputData.redirect_uri}}',
           response_type: 'code',
         },
       },
